3890d2f6cc64cbd16362d61000b3fac0ba288e7f
Lehlohonolo Mofula <54318202+LehlohonoloMofula@users.noreply.github.com>
2024-06-12 20:40:36 +0200

    Update practical_1.py

---
+++ Practical 1/practical_1.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+
+print("\nThis program was written by: ", name, surname)
+
+print("")
+print("Square")
+print("")
+print("* * * *")
+print("* * * *")
+print("* * * *")
+print("* * * *")
+print("* * * *")
+
+print("")
+print("Diamond")
+print("")
