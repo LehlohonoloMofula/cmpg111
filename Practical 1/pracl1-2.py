00f01eba4ac206c8453c3c8b1a7ccbb195f0d27f
Lehlohonolo Mofula <54318202+LehlohonoloMofula@users.noreply.github.com>
2024-06-12 20:41:42 +0200

    Create pracl1-2.py

---
+++ Practical 1/pracl1-2.py
@@ -0,0 +1,8 @@
+symbol = input("Please enter a symbol: ")
+
+print("\nTriangle\n")
+print(symbol)
+print(symbol, symbol)
+print(symbol, symbol, symbol)
+print(symbol, symbol, symbol, symbol)
+print(symbol, symbol, symbol, symbol, symbol)
